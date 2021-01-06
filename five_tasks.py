81956377c75342f6cd90c7c3321345210dc06f76
Hamlet Cynero <hamlet.avetikyn@gmail.com>
2021-01-06 17:13:08 +0300

    add five tasks file

---
+++ five_tasks.py
@@ -0,0 +1,56 @@
+''' five tasks
+'''
+
+
+#  FIRST TASK:
+#  Write three functions that sum the numbers
+#  in a given list using a for loop, a while loop,
+#  and recursion.
+def FirstTask():
+    pass
+
+
+#  SECOND TASK:
+#  Write a function that concatenates two lists
+#  by alternating elements. For example, given
+#  the two lists [a, b, c] and [1, 2, 3], the function
+#  should return [a, 1, b, 2, c, 3].
+def SecondTask():
+    pass
+
+
+#  THIRD TASK:
+#  Write a function that calculates the first 100
+#  Fibonacci numbers. By definition, the first two numbers
+#  in the Fibonacci sequence are 0 and 1, and each
+#  subsequent number is the sum of the previous two.
+#  As an example, here are the first 10 Fibonacci
+#  numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21 and 34.
+def ThirdTask():
+    pass
+
+
+#  FOURTH TASK:
+#  Write a function that takes a list of non-negative
+#  integers and orders them so that they are as large
+#  as possible. For example, [50, 2, 1, 9], the largest
+#  generated number is 95021.
+def FourthTask():
+    pass
+
+
+#  FIFTH TASK:
+#  Write a program that puts +, -, or a space in
+#  between 1, 2, ..., 9 (in that order) so that the
+#  result is 100. For example:
+#    1 + 2 + 34 - 5 + 67 - 8 + 9 = 100.
+def FifthTask():
+    pass
+
+#main function
+def main():
+    pass
+
+#init main function
+if __name__ == "__main__":
+    main()
